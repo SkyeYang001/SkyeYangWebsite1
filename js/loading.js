a8e00f04fca5b7442ecdce31f466212ba5c4bf33
Skye's Fighting Eggs <143848467+SkyeYang001@users.noreply.github.com>
2025-02-26 17:38:30 +0000

    Wed, Feb 26, 2025, 9:38 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -19,6 +19,6 @@ function blurring() {
     loadText.innerText = `${load}%`
     // going to the css sheet
     // backticks (`)
-    bg.style.filter = `blur(${50 - (load/2)}px)`
-    loadText.style.opacity = `${load/100}`
+    bg.style.filter = `blur(${50 - (load / 2)}px)`
+    loadText.style.opacity = `${1 - load / 100}`
 }
