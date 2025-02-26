c175f581e17d60fb2671135caebf0ed7c8d1ef44
Skye's Fighting Eggs <143848467+SkyeYang001@users.noreply.github.com>
2025-02-26 17:23:39 +0000

    Wed, Feb 26, 2025, 9:23 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -4,10 +4,17 @@ const bg = document.querySelector('.bg')
 // variable
 let load = 0
 
+// calling the function many times
+// the time is in millseconds
+let int = setInterval(blurring, 300)
+
+
 function blurring() {
     load = load + 1
+    // if statment ending the interval
+    if (load > 90) {
+        clearInterval(int)
+    }
     // backticks (`) turning int to string
     loadText.innerText = `${load}%`
-
-
 }
