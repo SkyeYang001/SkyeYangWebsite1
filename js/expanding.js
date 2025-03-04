f9b0fb9fb3a24442a467bff89f082ed3677fbd74
Skye's Fighting Eggs <143848467+SkyeYang001@users.noreply.github.com>
2025-03-04 00:32:48 +0000

    Mon, Mar 3, 2025, 4:32 PM -08:00

---
+++ js/expanding.js
@@ -0,0 +1,14 @@
+panels = document.querySelector('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panels.classList.remove('active')
+    })
+}
