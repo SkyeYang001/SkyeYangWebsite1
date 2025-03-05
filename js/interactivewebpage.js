035a794176ea2d0fd77413df2964dde6e33cd897
Skye's Fighting Eggs <143848467+SkyeYang001@users.noreply.github.com>
2025-03-05 17:39:01 +0000

    Wed, Mar 5, 2025, 9:39 AM -08:00

--- js/interactivewebpage.js
+++ js/interactivewebpage.js
@@ -3,3 +3,6 @@ const event2 = document.getElementById('event2')
 const event3 = document.getElementById('event3')
 const event4 = document.getElementById('event4')
 
+event4.addEventListener('click', () => {
+    console.log("Your good to go!")
+})
