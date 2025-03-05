259c00f8f1cc7f9e625db1648a825ba6469dfb54
Skye's Fighting Eggs <143848467+SkyeYang001@users.noreply.github.com>
2025-03-05 17:34:30 +0000

    Wed, Mar 5, 2025, 9:34 AM -08:00

--- js/interactivewebpage.js
+++ js/interactivewebpage.js
@@ -1,1 +1,5 @@
-const 
+const event1 = document.getElementById('event1')
+const event2 = document.getElementById('event2')
+const event3 = document.getElementById('event3')
+const event4 = document.getElementById('event4')
+
