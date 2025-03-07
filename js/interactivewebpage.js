5b1320135c1052840924c51ae9269b5cf0d797ee
Skye's Fighting Eggs <143848467+SkyeYang001@users.noreply.github.com>
2025-03-07 17:06:17 +0000

    Fri, Mar 7, 2025, 9:06 AM -08:00

--- js/interactivewebpage.js
+++ js/interactivewebpage.js
@@ -3,6 +3,39 @@ const event2 = document.getElementById('event2')
 const event3 = document.getElementById('event3')
 const event4 = document.getElementById('event4')
 
+event1.addEventListener('mouseover', () => {
+    event1.style.backgroundColor = "#e3be07"
+})
+
+event1.addEventListener('mouseout', () => {
+    event1.style.backgroundColor = "white"
+})
+
+event2.addEventListener('mouseover', () => {
+    event2.style.backgroundColor = "#e3be07"
+})
+
+event2.addEventListener('mouseout', () => {
+    event2.style.backgroundColor = "white"
+})
+
+event3.addEventListener('mouseover', () => {
+    event3.style.backgroundColor = "#e3be07"
+})
+
+event3.addEventListener('mouseout', () => {
+    event3.style.backgroundColor = "white"
+})
+
+event4.addEventListener('mouseover', () => {
+    event4.style.backgroundColor = "#f7eb0a"
+})
+
+event4.addEventListener('mouseout', () => {
+    event4.style.backgroundColor = "white"
+})
+
+
 event4.addEventListener('click', () => {
     submit()
 })
