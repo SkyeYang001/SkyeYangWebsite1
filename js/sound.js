35aa3ca774cc05d26a08a73908c2dc94af8e1eda
Skye's Fighting Eggs <143848467+SkyeYang001@users.noreply.github.com>
2025-03-11 17:19:19 +0000

    Tue, Mar 11, 2025, 10:19 AM -07:00

--- js/sound.js
+++ js/sound.js
@@ -5,6 +5,8 @@ sounds.forEach(sound = > {
     const btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerHTML = sound
-
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
     document.getElementById('buttons').appendChild(btn)
 })
