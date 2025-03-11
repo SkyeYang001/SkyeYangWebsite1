597103600041dae1f5567286747003a5511f131a
Skye's Fighting Eggs <143848467+SkyeYang001@users.noreply.github.com>
2025-03-11 17:16:10 +0000

    Tue, Mar 11, 2025, 10:16 AM -07:00

---
+++ js/sound.js
@@ -0,0 +1,10 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+// list.loop(singular variable)
+sounds.forEach(sound = > {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerHTML = sound
+
+    document.getElementById('buttons').appendChild(btn)
+})
