1bd6cce6dd3cdb7df70b9aaf250ecafd772aa0bd
Skye's Fighting Eggs <143848467+SkyeYang001@users.noreply.github.com>
2025-03-19 16:51:20 +0000

    Wed, Mar 19, 2025, 9:51 AM -07:00

---
+++ js/insect.js
@@ -0,0 +1,8 @@
+const screens = document.querySelectorAll('.screen')
+const start_btn = document.getElementById('start-btn')
+const choose_insects_btns = document.querySelectorAll('.choose-insect-btn')
+const game_container = document.getElementById('game-container')
+const time = document.getElementById('time')
+const score = document.getElementById('score')
+const message = document.getElementById('message')
+
