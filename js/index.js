628aac67ce8e1b08f77b464e0e76f0ee65644c2c
Skye's Fighting Eggs <143848467+SkyeYang001@users.noreply.github.com>
2025-03-28 10:19:09 -0700

    index, insect, and nav finished

--- js/index.js
+++ js/index.js
@@ -1,16 +1,36 @@
-const hero = document.getElementsByClassName('hero')
+const hero = document.getElementById('hero')
 const element1 = document.getElementById('element1')
 const element2 = document.getElementById('element2')
 const element3 = document.getElementById('element3')
+const pages = ['nav.html', 'search.html', 'loading.html', 'expanding.html', 'events.html', 'interactivewebpage.html', 'sound.html', 'insect.html']
 
-let active = false
+
+let pictureactive = false
 
 element1.addEventListener('click', () => {
-    if (active == false) {
+    if (pictureactive == false) {
         hero.classList.add('active')
-        active = true
+        pictureactive = true
     } else {
         hero.classList.remove('active')
-        active = false
+        pictureactive = false
+    }
+})
+
+element2.addEventListener('click', () => {
+    const x = Math.floor(Math.random() * pages.length)
+    window.location.href = pages[x]
+})
+
+
+let musicactive = false
+
+element3.addEventListener('click', () => {
+    if (musicactive == true) {
+        document.getElementById(music).pause()
+        // document.getElementById(music).currentTime = 0;
+    } else {
+        document.getElementById('music').play()
+        musicactive == true
     }
 })
