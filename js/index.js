82e74209ef024f0b59c6457756e238205fb163ac
Skye's Fighting Eggs <143848467+SkyeYang001@users.noreply.github.com>
2025-03-29 00:09:01 +0000

    Fri, Mar 28, 2025, 5:09 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const hero = document.getElementById('hero')
+const hello = document.getElementById('hello')
+const box = document.getElementsByClassName('box')
 const element1 = document.getElementById('element1')
 const element2 = document.getElementById('element2')
 const element3 = document.getElementById('element3')
@@ -10,9 +12,13 @@ let pictureactive = false
 element1.addEventListener('click', () => {
     if (pictureactive == false) {
         hero.classList.add('active')
+        hello.classList.add('active')
+        box.classList.add('active')
         pictureactive = true
     } else {
         hero.classList.remove('active')
+        hello.classList.remove('active')
+        box.classList.remove('active')
         pictureactive = false
     }
 })
