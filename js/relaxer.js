e8ad1cf787186b06b05733aeff44eceaaec69e40
Skye's Fighting Eggs <143848467+SkyeYang001@users.noreply.github.com>
2025-03-31 16:58:44 +0000

    Mon, Mar 31, 2025, 9:58 AM -07:00

--- js/relaxer.js
+++ js/relaxer.js
@@ -4,4 +4,14 @@ const text = document.getElementbyId('text')
 function breathAnimation() {
     text.innerHTML = 'Breath In'
     container.classList.add('grow')
+
+    setTimeout( () => {
+        text.innerHTML = 'Hold'
+    }, 3000)
+
+    setTimeout( () => {
+        text.innerHTML = 'Breath Out'
+        container.classList.remove('grow')
+        container.classList.add('shrink')
+    }, 4500)
 }
